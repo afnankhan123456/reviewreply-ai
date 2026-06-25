b2c7951e491a6763f23258e78ea404e4b7973627
afnan khan <afnankhan529683@gmail.com>
2026-06-25 13:19:03 +0530

    Update route.ts

--- app/api/save-review/route.ts
+++ app/api/save-review/route.ts
@@ -1,19 +1,28 @@
-import { prisma } from "@/lib/prisma";
+import { prisma } from "../../../lib/prisma";
 
 export async function GET() {
+  try {
 
-  const review =
-    await prisma.review.create({
-      data: {
-        author: "Afnan",
-        rating: 5,
-        comment: "Amazing service",
-        reply: "Thank you so much!",
-      },
+    const review =
+      await prisma.review.create({
+        data: {
+          author: "Afnan",
+          rating: 5,
+          comment: "Amazing service",
+          reply: "Thank you so much!",
+        },
+      });
+
+    return Response.json({
+      success: true,
+      review,
     });
 
-  return Response.json({
-    success: true,
-    review,
-  });
+  } catch (error: any) {
+
+    return Response.json({
+      success: false,
+      error: error.message,
+    });
+  }
 }
