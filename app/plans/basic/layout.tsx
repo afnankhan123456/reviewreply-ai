4c6af47ac255ecc714e44228f532f77745d87cc1
afnan khan <afnankhan529683@gmail.com>
2026-06-26 16:49:21 +0530

    Add files via upload

---
+++ app/plans/basic/layout.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import BasicSidebar from "./component/BasicSidebar";
+
+export default function BasicLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen bg-zinc-100 dark:bg-black text-black dark:text-white">
+
+      <BasicSidebar />
+
+      <main className="flex-1 p-8">
+        {children}
+      </main>
+
+    </div>
+  );
+}
+
+
