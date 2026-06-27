718c568712d2b181ae3565b06aeaa4ee991463b0
afnan khan <afnankhan529683@gmail.com>
2026-06-27 17:43:28 +0530

    Update layout.tsx

--- app/plans/basic/layout.tsx
+++ app/plans/basic/layout.tsx
@@ -1,23 +1,20 @@
 "use client";
 
-import BasicSidebar from "./component/BasicSidebar";
-
 export default function BasicLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex min-h-screen bg-zinc-100 dark:bg-black text-black dark:text-white">
+    <div className="min-h-screen bg-zinc-100 dark:bg-black text-black dark:text-white">
 
-      <BasicSidebar />
-
-      <main className="flex-1 p-8">
+      <main className="p-8">
         {children}
       </main>
 
     </div>
   );
 }
+
 
 
