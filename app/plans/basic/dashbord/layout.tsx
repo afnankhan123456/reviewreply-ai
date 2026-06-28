7768ac504fc798178128c688c8ee1052d01b1bff
afnan khan <afnankhan529683@gmail.com>
2026-06-28 10:08:54 +0530

    Add files via upload

---
+++ app/plans/basic/dashbord/layout.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import Sidebar from "./components/Sidebar";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="flex min-h-screen bg-[#F5F7FB]">
+
+      {/* SIDEBAR */}
+
+      <Sidebar />
+
+      {/* MAIN CONTENT */}
+
+      <main className="flex-1 p-6 overflow-y-auto">
+        {children}
+      </main>
+
+    </div>
+  );
+}
