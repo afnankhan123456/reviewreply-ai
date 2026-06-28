487af3db4ed7f86bc0cb741f6a7f032489b45c5c
afnan khan <afnankhan529683@gmail.com>
2026-06-28 12:04:44 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
+"use client";
+
 import "./globals.css";
 import Providers from "../components/Providers";
+
+import { useEffect, useState } from "react";
 
 export const metadata = {
   title: "ReviewReply AI",
@@ -11,13 +15,42 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+
+    const savedMode = localStorage.getItem("darkMode");
+
+    if (savedMode === "true") {
+
+      document.documentElement.classList.add("dark");
+
+    } else {
+
+      document.documentElement.classList.remove("dark");
+
+    }
+
+    setMounted(true);
+
+  }, []);
+
+  if (!mounted) return null;
+
   return (
+
     <html lang="en">
-      <body>
+
+      <body className="bg-zinc-50 dark:bg-zinc-900 text-zinc-900 dark:text-white transition duration-300">
+
         <Providers>
           {children}
         </Providers>
+
       </body>
+
     </html>
+
   );
 }
