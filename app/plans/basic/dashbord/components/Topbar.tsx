5b7bd581110a9d03523a9bd1b99477dff09c2c11
afnan khan <afnankhan529683@gmail.com>
2026-06-30 19:12:30 +0530

    Update Topbar.tsx

--- app/plans/basic/dashbord/components/Topbar.tsx
+++ app/plans/basic/dashbord/components/Topbar.tsx
@@ -18,7 +18,7 @@ export default function Topbar() {
 
         <button
           onClick={() => setOpen(!open)}
-          className="flex items-center gap-3 bg-white border border-zinc-200 rounded-xl px-4 py-2 hover:bg-zinc-50 transition"
+          className="flex items-center gap-3 bg-white dark:bg-[#111827] border border-zinc-200 dark:border-zinc-700 rounded-xl px-4 py-2 hover:bg-zinc-50 dark:hover:bg-zinc-800 transition-colors duration-300"
         >
 
           <img
@@ -28,9 +28,11 @@ export default function Topbar() {
           />
 
           <div className="flex flex-col items-start">
-            <span className="text-sm font-semibold text-black">
+
+            <span className="text-sm font-semibold text-black dark:text-white">
               {session?.user?.name}
             </span>
+
           </div>
 
         </button>
@@ -38,11 +40,11 @@ export default function Topbar() {
         {/* DROPDOWN */}
 
         {open && (
-          <div className="absolute right-0 mt-2 w-40 bg-white border border-zinc-200 rounded-xl shadow-lg p-2 z-50">
+          <div className="absolute right-0 mt-2 w-40 bg-white dark:bg-[#111827] border border-zinc-200 dark:border-zinc-700 rounded-xl shadow-lg p-2 z-50 transition-colors duration-300">
 
             <button
               onClick={() => signOut()}
-              className="w-full text-left px-3 py-2 rounded-lg hover:bg-zinc-100 text-sm font-medium text-red-500"
+              className="w-full text-left px-3 py-2 rounded-lg hover:bg-zinc-100 dark:hover:bg-zinc-800 text-sm font-medium text-red-500"
             >
               Logout
             </button>
