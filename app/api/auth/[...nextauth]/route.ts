611dd263054cbebbcb3c894c2235b8208ac8db6e
afnan khan <afnankhan529683@gmail.com>
2026-07-01 14:12:33 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { prisma } from "@/lib/prisma";
+
 export const runtime = "nodejs";
 
 const adminEmail = process.env.ADMIN_EMAIL;
@@ -14,7 +16,48 @@ const handler = NextAuth({
   ],
 
   callbacks: {
+
+    async signIn({ user }) {
+
+      if (!user.email) {
+        return false;
+      }
+
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          email: user.email,
+        },
+      });
+
+      if (!existingUser) {
+
+        await prisma.user.create({
+          data: {
+            name: user.name,
+            email: user.email,
+            image: user.image,
+            plan: "basic",
+          },
+        });
+
+      } else {
+
+        await prisma.user.update({
+          where: {
+            email: user.email,
+          },
+          data: {
+            lastLogin: new Date(),
+          },
+        });
+
+      }
+
+      return true;
+    },
+
     async jwt({ token, user }) {
+
       if (user?.email) {
         token.isAdmin = user.email === adminEmail;
       }
@@ -23,6 +66,7 @@ const handler = NextAuth({
     },
 
     async redirect({ baseUrl, url }) {
+
       if (url.includes("admin=true")) {
         return `${baseUrl}/admin`;
       }
@@ -39,6 +83,3 @@ const handler = NextAuth({
 });
 
 export { handler as GET, handler as POST };
-
-
-
