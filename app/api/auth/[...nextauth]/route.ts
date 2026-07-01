f6d5fa1abc75d937c19b009f35a5954408638d48
afnan khan <afnankhan529683@gmail.com>
2026-07-01 15:17:16 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+import { prisma } from "../../../../lib/prisma";
+
 export const runtime = "nodejs";
 
 const adminEmail = process.env.ADMIN_EMAIL;
@@ -14,6 +16,45 @@ const handler = NextAuth({
   ],
 
   callbacks: {
+
+    async signIn({ user }) {
+
+      if (!user.email) {
+        return false;
+      }
+
+      const existingUser = await prisma.user.findUnique({
+        where: {
+          email: user.email,
+        },
+      });
+
+      if (!existingUser) {
+
+        await prisma.user.create({
+          data: {
+            name: user.name,
+            email: user.email,
+            image: user.image,
+            plan: "basic",
+          },
+        });
+
+      } else {
+
+        await prisma.user.update({
+          where: {
+            email: user.email,
+          },
+          data: {
+            lastLogin: new Date(),
+          },
+        });
+
+      }
+
+      return true;
+    },
 
     async jwt({ token, user }) {
 
