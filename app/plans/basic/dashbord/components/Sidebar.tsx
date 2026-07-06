2c1b01b71def6793a0a9e5dcf9dee32d53c2bf7f
afnan khan <afnankhan529683@gmail.com>
2026-07-06 14:47:00 +0530

    Update Sidebar.tsx

--- app/plans/basic/dashbord/components/Sidebar.tsx
+++ app/plans/basic/dashbord/components/Sidebar.tsx
@@ -78,7 +78,7 @@ export default function Sidebar() {
   ];
 
   return (
-    <div className="w-[260px] bg-white dark:bg-[#111827] border-r border-zinc-200 dark:border-zinc-800 flex flex-col justify-between p-5 transition-colors duration-300">
+    <div className="hidden lg:flex w-[260px] bg-white dark:bg-[#111827] border-r border-zinc-200 dark:border-zinc-800 flex-col justify-between p-5 transition-colors duration-300">
 
       <div>
 
