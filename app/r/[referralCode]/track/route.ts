48ef5879690f5aa8907c76125b171e98d5ecf891
afnan khan <afnankhan529683@gmail.com>
2026-07-08 11:17:52 +0530

    Update route.ts

--- app/r/[referralCode]/track/route.ts
+++ app/r/[referralCode]/track/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import { prisma } from "../../../../lib/prisma";
 import { getServerSession } from "next-auth";
-import { GET as authHandler } from "../../api/auth/[...nextauth]/route";
+import { GET as authHandler } from "../../../api/auth/[...nextauth]/route";
 
 export async function GET(
   request: Request,
