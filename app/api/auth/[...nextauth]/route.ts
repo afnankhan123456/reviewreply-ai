e52ed3b2bb5414cd04b3006678f7e787e99dd75a
afnan khan <afnankhan529683@gmail.com>
2026-07-15 16:10:46 +0530

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,3 +6,6 @@ export const runtime = "nodejs";
 const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
+
+// ✅ YE LINE ADD KARO (Isse error fix ho jayega)
+export { authOptions };
