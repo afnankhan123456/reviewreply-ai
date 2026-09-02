a06b91f8b35827729da4e9c64ed8464602129f62
afnan khan <afnankhan529683@gmail.com>
2026-09-02 16:38:04 +0530

    Update LoginUI.tsx

--- components/LoginUI.tsx
+++ components/LoginUI.tsx
@@ -154,7 +154,7 @@ function LoginHero() {
             </p>
           </div>
 
-          <div className="w-full max-w-[24rem] shrink-0 -translate-y-4">
+          <div className="w-full max-w-[25rem] shrink-0 -translate-y-4">
             <GoogleCard />
           </div>
         </div>
